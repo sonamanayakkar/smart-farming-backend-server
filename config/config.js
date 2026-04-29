0243c9e1c56ee57501250bdfd38016fb975babfb
sonamanayakkar <esonaman2@gmail.com>
2026-04-29 22:10:07 +0530

    atlas connected

--- config/config.js
+++ config/config.js
@@ -1,7 +1,18 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 let dbconnection = async (req, res) => {
-    await mongoose.connect('mongodb://127.0.0.1:27017/smart_farming')
+
+    let env = process.env.NODE_ENV
+
+    if (env == "local") {
+        await mongoose.connect('mongodb://127.0.0.1:27017/smart_farming')
+    } else {
+
+        await mongoose.connect('mongodb+srv://esonaman2_db_user:RKPocox37@cluster0.ugz4ntp.mongodb.net/smart_farming')
+    }
+
+
 }
 
 dbconnection().then(() => console.log('mongodb connected')).catch(err => console.log(err))
